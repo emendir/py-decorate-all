f494101c03954638f2dabc1d0214f6e897c6c82e
emendir <emendir@earth.milkyway>
2024-04-22 14:43:18 +0200

    readme and metadata update

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='decorate_all',
-    version='0.1.0',
+    version='0.1.1',
     packages=['decorate_all'],
     url='https://github.com/emendir/py-decorate-all',
-    description='A short description of your package',
+    description='Apply a decorator to all functions in a module.',
     long_description=open('ReadMe.md').read(),
     long_description_content_type='text/markdown',
 )
